9de6c0e142c944050aa59e35f42ad33598ec6fc0
Nathan Edwards <npfedwards@gmail.com>
2012-10-30 12:29:02 +0000

    huh

--- Combinations.hs
+++ Combinations.hs
@@ -16,11 +16,10 @@ module Combinations (
     nCr,
     nPr
 ) where
-    import IntegerMaths as IM
     nPr :: Integer -> Integer -> Integer
     nPr 0 r = 1
     nPr n r = foldl (*) 1 [n-r+1..n]
 
     nCr :: Integer -> Integer -> Integer
     nCr 0 r = 1
-    nCr n r = IM.division (nPr n r) (foldl (*) 1 [1..r])
+    nCr n r = quot (nPr n r) (foldl (*) 1 [1..r])
